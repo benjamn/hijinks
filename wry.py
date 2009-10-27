8318356e2972998f0364081f588bc5e64b677f6b
Ben Newman <ben@debian-dev.(none)>
2009-10-27 19:38:59 +0000

    Avoiding backend requests if the Accept request header does not contain html.

--- wry.py
+++ wry.py
@@ -14,7 +14,7 @@ _host_exp = re.compile(
     r"\.wry\.ly" +
     r"(?P<port>:\d+)?")
 
-_head_exp = re.compile(r"</head>", re.I)
+_body_exp = re.compile(r"</body>", re.I)
 
 define("port", default=8888, help="run on the given port", type=int)
 
@@ -22,16 +22,22 @@ class WryHandler(tornado.web.RequestHandler):
 
     def _parse(self):
         m = _host_exp.match(self.request.host)
+        q = self.request.query
+        q = q and ("?" + q)
         return {
             "host": m.group("host"),
             "port": m.group("port") or "",
-            "path": "?".join([self.request.path,
-                              self.request.query])
+            "path": self.request.path + q
         } if m else None
 
     def _fail_wryly(self):
         self.write("My, my. Aren't we feeling wry?")
         self.finish();
+
+    def _redirect(self, url):
+        self.set_status(301)
+        self.set_header("Location", url)
+        self.finish()
 
     @tornado.web.asynchronous
     def get(self):
@@ -39,8 +45,12 @@ class WryHandler(tornado.web.RequestHandler):
         if parsed is None:
             return self._fail_wryly()
         url = "http://%(host)s%(port)s%(path)s" % parsed
-        tornado.httpclient.AsyncHTTPClient().fetch(url,
-            callback=self.async_callback(self.on_response, url))
+        hdrs = self.request.headers
+        if "Accept" in hdrs and "html" not in hdrs["Accept"].lower():
+            self._redirect(url)
+        else:
+            tornado.httpclient.AsyncHTTPClient().fetch(url,
+                callback=self.async_callback(self.on_response, url))
 
     def on_response(self, url, response):
         if response.error:
@@ -49,13 +59,12 @@ class WryHandler(tornado.web.RequestHandler):
         if "html" in ct:
             self.set_header("Content-Type", ct)
             self.write(re.sub(
-                _head_exp,
-                "<script src='http://static.wry.ly/frag.js'></script></head>",
+                _body_exp,
+                "<script src='http://static.wry.ly/frag.js'></script></body>",
                 response.body))
+            self.finish()
         else:
-            self.set_status(301)
-            self.set_header("Location", url)
-        self.finish()
+            self._redirect(url)
 
 application = tornado.web.Application([
     (r".*", WryHandler),
